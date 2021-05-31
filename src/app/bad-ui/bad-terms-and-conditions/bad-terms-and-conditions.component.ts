2c6cfb3f175e6f36a29ea8ae66062f7897c515c8
Benjamin Yocum <waerstein@gmail.com>
2021-05-31 16:35:10 -0700

    Fix some text in the Terms and Conditions

--- src/app/bad-ui/bad-terms-and-conditions/bad-terms-and-conditions.component.ts
+++ src/app/bad-ui/bad-terms-and-conditions/bad-terms-and-conditions.component.ts
@@ -9,7 +9,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class BadTermsAndConditionsComponent implements OnInit {
 
   currentText: string = '';
-  terms: string = `These Terms and Conditions constitute a legally binding agreement made between you, whether personally or on behalf of an entity (“you”) and [business entity name] (“we,” “us” or “our”), concerning your access to and use of the website as well as any other media form, media channel, mobile website or mobile application related, linked, or otherwise connected thereto (collectively, the “Site”).
+  terms: string = `These Terms and Conditions constitute a legally binding agreement made between you, whether personally or on behalf of an entity (“you”) and benyocum.com (“we,” “us” or “our”), concerning your access to and use of the website as well as any other media form, media channel, mobile website or mobile application related, linked, or otherwise connected thereto (collectively, the “Site”).
 
 You agree that by accessing the Site, you have read, understood, and agree to be bound by all of these Terms and Conditions. If you do not agree with all of these Terms and Conditions, then you are expressly prohibited from using the Site and you must discontinue use immediately.
 
